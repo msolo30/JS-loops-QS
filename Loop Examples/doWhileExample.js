f2145dc5430e54bf46be3a35ffcfaf2a03e4a6dd
Sophia <sophiam2702@tuckahoeschools.org>
2024-12-04 13:32:23 +0000

    while ex and dowhile ex

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,24 @@
+let outDiv = document.getElementById("output");
+let passowrdInput = document.getElementById("passwordInput");
+let submitButton = document.getElementById("sumbitButton");
 
+const correctPassword = "1234";
+outputDiv.innerHTML += "<h2>Using a password prompt</h2>";
+
+submitButton.addEventListener("click", () =>{
+    let password;
+    let attempt = 0;
+
+    do{
+        password = passwordInput.value;
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "Access granted!<br>";
+            passwordInput.disabled = true;
+            submitButton.disabled = true;
+            break;
+        }else{
+            
+        }
+    }
+});
